bdcf808ac8775da83e7034faa589d52bd3eb73ba
Andrey Kudinov <2499931@gmail.com>
2021-10-17 00:54:06 +0400

    update prerander

--- tools/prerender.ts
+++ tools/prerender.ts
@@ -7,7 +7,7 @@ const aliases: Record<string, string> = {}
 const configAliases = config.resolve.alias as Alias[]
 
 configAliases.forEach(({ find, replacement }) => {
-    if (typeof find === 'string') aliases[find] = replacement
+  if (typeof find === 'string') aliases[find] = replacement
 })
 
 addAliases(aliases)
@@ -18,54 +18,56 @@ import { renderToString } from 'react-dom/server'
 import { writeFile, readFileSync } from 'fs'
 import { minify } from 'html-minifier'
 
-import { map } from '../src/pages'
+import { map } from '../src/pages/pages'
 
 const template = readFileSync('./dist/index.html')
 const prerenders: Promise<string>[] = []
 
 const remapPage = {
-    '/': 'landing',
-    default: '404'
+  '/': 'home',
+  '/articles': 'articles',
+  '/about': 'about',
+  '/hire': 'hire',
+  '/articles/grid': 'grid',
+  default: '404'
 } as const
 
 const blackList = ['default'] as const
 
 const main = async () => {
-    Object.keys(map).forEach(async (page) => {
-        const pageName = remapPage[page] || page
+  Object.keys(map).forEach(async (page) => {
+    const pageName = remapPage[page] || page
 
-        if (blackList.includes(pageName)) return
+    if (blackList.includes(pageName)) return
 
-        const { default: component } = require(`../src/pages/${pageName}.tsx`)
+    const { default: component } = require(`../src/pages/${pageName}.tsx`)
 
-        const markup: string =
-            typeof component === 'string'
-                ? component
-                : renderToString(component())
+    const markup: string =
+      typeof component === 'string' ? component : renderToString(component())
 
-        let prerender = `${template}`.replace('<!-- app -->', markup)
-        prerender = minify(prerender)
+    let prerender = `${template}`.replace('<!-- app -->', markup)
+    prerender = minify(prerender)
 
-        prerenders.push(
-            new Promise(async (resolve, reject) => {
-                let fileName = pageName
+    prerenders.push(
+      new Promise(async (resolve, reject) => {
+        let fileName = pageName
 
-                if (fileName === 'landing') fileName = 'index'
+        if (fileName === 'landing') fileName = 'index'
 
-                writeFile(`./dist/${fileName}.html`, prerender, (error) => {
-                    if (error) reject(pageName)
-                    resolve(pageName)
-                })
-            })
-        )
-    })
+        writeFile(`./dist/${fileName}.html`, prerender, (error) => {
+          if (error) reject(pageName)
+          resolve(pageName)
+        })
+      })
+    )
+  })
 
-    try {
-        await Promise.all(prerenders)
-    } catch (page) {
-        console.warn(`Failed to prerender on page ${page}`)
-        process.exit(1)
-    }
+  try {
+    await Promise.all(prerenders)
+  } catch (page) {
+    console.warn(`Failed to prerender on page ${page}`)
+    process.exit(1)
+  }
 }
 
 main()
