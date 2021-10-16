fcf10600493a72ddd798dbd7596be3fa13529e8f
Andrey Kudinov <2499931@gmail.com>
2021-10-17 00:53:00 +0400

    comment out Scope

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { ActiveLink, Scope } from '@atoms'
 
 const Articles = () => (
-  <Scope base="/articles">
-    <ActiveLink href="/grid">Grid</ActiveLink>
-  </Scope>
+  // <Scope base="/articles">
+  //   <ActiveLink href="/grid">Grid</ActiveLink>
+  // </Scope>
+  <span>Articles</span>
 )
 
 export default Articles
