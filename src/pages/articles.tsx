0ff95a4d2c3cfee476d8f91631db33b2f20d36cc
Andrey Kudinov <2499931@gmail.com>
2021-10-19 09:00:09 +0400

    temporarily add <a> instead of a Link

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,6 +1,6 @@
-import { Header } from '@atoms'
+// import { Header } from '@atoms'
 import React from 'react'
-import { Link, useLocation } from 'wouter'
+// import { Link, useLocation } from 'wouter'
 
 const Articles = () => {
   // const [location, setLocation] = useLocation(); 
@@ -9,7 +9,8 @@ const Articles = () => {
     <>
       <span>Articles</span>
       {/* <Header /> */}
-      <Link href="/articles/grid">Grid</Link>
+      {/* <Link href="/articles/grid">Grid</Link> */}
+      <a href="/articles/grid">Grid</a>
       {/* <a onClick={() => setLocation("/articles/grid")}>Click to update</a> */}
     </>
   )
