6367e246f5e58fd9b9d498e8f993f8a165b23f7c
Andrey Kudinov <2499931@gmail.com>
2021-10-20 23:38:55 +0400

    clean up articles

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,17 +1,13 @@
 // import { Header } from '@atoms'
 import React from 'react'
-import { Link, useLocation } from 'wouter'
+import { Link } from 'wouter'
 
 const Articles = () => {
-  // const [location, setLocation] = useLocation(); 
 
   return (
     <>
       <span>Articles</span>
-      {/* <Header /> */}
       <Link href="/articles/grid">Grid</Link>
-      {/* <a href="/articles/grid">Grid</a> */}
-      {/* <a onClick={() => setLocation("/articles/grid")}>Click to update</a> */}
     </>
   )
 }
