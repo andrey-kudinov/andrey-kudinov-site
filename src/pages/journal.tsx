ab71d604d509066061064fae14aa90ea0d25f448
Andrey Kudinov <2499931@gmail.com>
2021-10-24 12:35:12 +0400

    add styles and some interesting things

--- src/pages/journal.tsx
+++ src/pages/journal.tsx
@@ -9,32 +9,65 @@ const Journal = () => {
     }
     const daysArray = []
     for (let date = new Date(start); date <= end; date.setDate(date.getDate() + 1)) {
-      daysArray.push(new Intl.DateTimeFormat('ru-RU', options).format(date));
+      daysArray.push({
+        day: new Intl.DateTimeFormat('ru-RU', { day: '2-digit' }).format(date),
+        dayWeek: new Intl.DateTimeFormat('ru-RU', { weekday: 'short' }).format(date),
+      });
     }
     return daysArray;
   };
 
   const days = getDaysArray(new Date('Oct 22, 2021'), new Date('Nov 22, 2021'))
 
+  const cellClass = 'whitespace-nowrap py-2 px-2 border border-solid border-black cell'
+
   const daysRender = days.map(day => {
     return (
-      <td className='p-2'>{day}</td>
+      day.day === '01'
+        ? <td className={`text-green-600 font-bold ${cellClass}`}>{day.day}</td>
+        : day.dayWeek === 'сб' || day.dayWeek === 'вс'
+          ? <td className={`text-red-600 font-bold ${cellClass}`}>{day.day}</td>
+          : <td className={`font-bold ${cellClass}`}>{day.day}</td>
     )
   })
 
+  daysRender.unshift(
+    <td className={`first-col ${cellClass}`}>
+      &nbsp;
+    </td>
+  )
+
   const rows = []
+  let i = 0
+  const labels = [
+    'Lessons with a tutor',
+    'Small talks',
+    'Self-study English',
+    'Pair coding',
+    'React',
+    'Algorithms',
+    'Shopify',
+    'Side projects'
+  ]
 
-  for (let i = 0; i < 7; i++) {
+  while (i < labels.length) {
     const row = []
+    let n = 0
 
-    for (let n = 0; n < days.length; n++) {
+    while (n < days.length) {
       const random = Math.floor(Math.random() * 1e6);
+
       row.push(
-        <td className='py-4 px-2 border border-solid border-black'>
-          <input id={`input-${i}-${random}`} name="input" type="checkbox" />
-          <label htmlFor={`input-${i}-${random}`}></label>
-        </td>
+        <>
+          <td className={cellClass}>
+            &nbsp;
+            <input id={`input-${i}-${random}`} name="input" type="checkbox" />
+            <label htmlFor={`input-${i}-${random}`}></label>
+          </td>
+        </>
       )
+
+      n++
     }
 
     rows.push(
@@ -42,22 +75,26 @@ const Journal = () => {
         {row}
       </tr>
     )
+
+    // add label
+    row.unshift(
+      <td className={`first-col ${cellClass}`}>
+        {labels[i]}
+      </td>
+    )
+
+    i++
   }
 
-  return (
-    <div className="journal w-full h-full flex">
-      <table>
-        <tr><td className='px-2'>Somethings</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Lessons with a tutor</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Small talks</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Self-study English</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Pair coding</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">React</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Algorithms</td></tr>
-        <tr><td className="whitespace-nowrap px-2 border-solid border-black">Shopify</td></tr>
-      </table>
 
-      <div className="pl-4 overflow-scroll">
+  return (
+    <div className="journal w-full h-full">
+      <p className='mb-4'>This is my development journal.</p>
+      <p className='mb-4'>When I do an action from the list, I will click on the cell. </p>
+      <p className='mb-4'>This will mark the action as completed for that day. </p>
+      <p className='mb-4'>The data will be saved in localStorage.</p>
+
+      <div className="overflow-scroll">
         <table>
           <tr>
             {daysRender}
