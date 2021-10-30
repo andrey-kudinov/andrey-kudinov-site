a5e5bdb4ab65e1030fc36d56b71d2c747d78a931
Andrey Kudinov <2499931@gmail.com>
2021-10-30 21:28:11 +0400

    update home page animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { SyntheticEvent, useRef } from 'react'
 
 const Main = () => {
   const parentRef = useRef<HTMLDivElement>(null)
@@ -8,7 +8,13 @@ const Main = () => {
   const blockFourRef = useRef<HTMLDivElement>(null)
   const blockCenterRef = useRef<HTMLDivElement>(null)
 
-  const handleBlockOne = (change: string, change2: any, change3: any) => {
+  const handleBlockOneOrFour = (
+    e: SyntheticEvent,
+    change: string,
+    change2: any,
+    change3: any
+  ) => {
+    e.stopPropagation()
     if (
       parentRef.current &&
       blockOneRef.current &&
@@ -61,7 +67,14 @@ const Main = () => {
     }
   }
 
-  const handleBlockTwo = (change: string, change2: any, change3: any) => {
+  const handleBlockTwoOrThree = (
+    e: SyntheticEvent,
+    change: string,
+    change2: any,
+    change3: any,
+    change4: any
+  ) => {
+    e.stopPropagation()
     if (
       parentRef.current &&
       blockOneRef.current &&
@@ -70,52 +83,47 @@ const Main = () => {
       blockFourRef.current &&
       blockCenterRef.current
     ) {
+      const { width: parentWidth, height: parentHeight } =
+        parentRef.current.getBoundingClientRect()
+      const { width: blockOneWidth, height: blockOneHeight } =
+        blockOneRef.current.getBoundingClientRect()
+      const { width: blockTwoWidth, height: blockTwoHeight } =
+        blockTwoRef.current.getBoundingClientRect()
+      const { width: blockThreeWidth, height: blockThreeHeight } =
+        blockThreeRef.current.getBoundingClientRect()
+      const { width: blockFourWidth, height: blockFourHeight } =
+        blockFourRef.current.getBoundingClientRect()
+
       blockOneRef.current.style.width = `calc(${Math.round(
-        (blockOneRef.current.getBoundingClientRect().width /
-          parentRef.current.getBoundingClientRect().width) *
-          100
+        (blockOneWidth / parentWidth) * 100
       )}% ${change})`
       blockOneRef.current.style.height = `calc(${Math.round(
-        (blockOneRef.current.getBoundingClientRect().height /
-          parentRef.current.getBoundingClientRect().height) *
-          100
-      )}% ${change}`
+        (blockOneHeight / parentHeight) * 100
+      )}% ${change2}`
 
       blockTwoRef.current.style.width = `calc(${Math.round(
-        (blockTwoRef.current.getBoundingClientRect().width /
-          parentRef.current.getBoundingClientRect().width) *
-          100
+        (blockTwoWidth / parentWidth) * 100
       )}% ${change2})`
       blockTwoRef.current.style.height = `calc(${
-        (blockTwoRef.current.getBoundingClientRect().height /
-          parentRef.current.getBoundingClientRect().height) *
-        100
-      }% ${change}`
+        (blockTwoHeight / parentHeight) * 100
+      }% ${change2}`
 
       blockThreeRef.current.style.width = `calc(${Math.round(
-        (blockThreeRef.current.getBoundingClientRect().width /
-          parentRef.current.getBoundingClientRect().width) *
-          100
+        (blockThreeWidth / parentWidth) * 100
       )}% ${change}`
       blockThreeRef.current.style.height = `calc(${Math.round(
-        (blockThreeRef.current.getBoundingClientRect().height /
-          parentRef.current.getBoundingClientRect().height) *
-          100
-      )}% ${change2}`
+        (blockThreeHeight / parentHeight) * 100
+      )}% ${change}`
 
       blockFourRef.current.style.width = `calc(${Math.round(
-        (blockFourRef.current.getBoundingClientRect().width /
-          parentRef.current.getBoundingClientRect().width) *
-          100
+        (blockFourWidth / parentWidth) * 100
       )}% ${change2}`
       blockFourRef.current.style.height = `calc(${Math.round(
-        (blockFourRef.current.getBoundingClientRect().height /
-          parentRef.current.getBoundingClientRect().height) *
-          100
-      )}% ${change2}`
+        (blockFourHeight / parentHeight) * 100
+      )}% ${change}`
 
       blockCenterRef.current.style.top = `${change3}`
-      blockCenterRef.current.style.left = `${change3}`
+      blockCenterRef.current.style.left = `${change4}`
     }
   }
 
@@ -124,12 +132,35 @@ const Main = () => {
       <div
         ref={blockOneRef}
         className="block block-1"
-        onMouseOver={() => handleBlockOne('+ 10%', '- 10%', '50%')}
-        onMouseOut={() => handleBlockOne('- 10%', '+ 10%', '40%')}
+        onMouseEnter={(e) => handleBlockOneOrFour(e, '+ 10%', '- 10%', '50%')}
+        onMouseLeave={(e) => handleBlockOneOrFour(e, '- 10%', '+ 10%', '40%')}
       ></div>
-      <div ref={blockTwoRef} className="block block-2"></div>
-      <div ref={blockThreeRef} className="block block-3"></div>
-      <div ref={blockFourRef} className="block block-4"></div>
+      <div
+        ref={blockTwoRef}
+        className="block block-2"
+        onMouseEnter={(e) =>
+          handleBlockTwoOrThree(e, '- 10%', '+ 10%', '50%', '30%')
+        }
+        onMouseLeave={(e) =>
+          handleBlockTwoOrThree(e, '+ 10%', '- 10%', '40%', '40%')
+        }
+      ></div>
+      <div
+        ref={blockThreeRef}
+        className="block block-3"
+        onMouseEnter={(e) =>
+          handleBlockTwoOrThree(e, '+ 10%', '- 10%', '30%', '50%')
+        }
+        onMouseLeave={(e) =>
+          handleBlockTwoOrThree(e, '- 10%', '+ 10%', '40%', '40%')
+        }
+      ></div>
+      <div
+        ref={blockFourRef}
+        className="block block-4"
+        onMouseEnter={(e) => handleBlockOneOrFour(e, '- 10%', '+ 10%', '30%')}
+        onMouseLeave={(e) => handleBlockOneOrFour(e, '+ 10%', '- 10%', '40%')}
+      ></div>
       <div ref={blockCenterRef} className="block block-center"></div>
     </div>
   )
