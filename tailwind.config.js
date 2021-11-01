36d86e65f6b27fc77cfe3b80415dcc5501351a46
Andrey Kudinov <2499931@gmail.com>
2021-11-01 20:47:35 +0400

    add flex

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,20 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
-      'max-md': {'max': '767px'},
+      'max-md': { max: '767px' }
+    },
+    flex: {
+      1: '1 1 0%',
+      2: '2 2 0%'
     }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')]
 }
